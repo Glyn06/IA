715426fac518556d086ac21bc6ba7df22c8c9af1
Jose Quintana Martin <jquintanamartin@hotmail.com>
2019-10-18 22:21:44 -0300

    Flocking

---
+++ Flocking/Assets/Boid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    FlockingManager fM;
+    public float speed = 2.5f;
+    public float turnSpeed = 5f;
+    public Vector2 currentPosition;
+    public Vector3 currentRotation;
+    public CircleCollider2D circleCollider2D;
+
+    private void Start()
+    {
+        fM = FlockingManager.instance;
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+        currentPosition = transform.position;
+        transform.up = Vector3.Lerp(transform.up, ACS(),turnSpeed* Time.deltaTime);
+    }
+
+    public Vector2 ACS() {
+
+        Vector2 ACS = fM.Alignment(this) + fM.Cohesion(this) + fM.Separation(this) + fM.Direction(this,fM.flockPoint);
+        ACS.Normalize();
+
+        return ACS;
+    }
+}
---
+++ Flocking/Assets/FlockingManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+    public static FlockingManager instance;
+    public GameObject flockPoint;
+    private Boid[] boids;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        boids = FindObjectsOfType<Boid>();
+    }
+    public Vector2 Alignment(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+        Vector2 avg = Vector2.zero;
+        foreach (Boid b in insideRadiusBoids)
+            avg += (Vector2)b.transform.up.normalized;
+        avg /= insideRadiusBoids.Count;
+        avg.Normalize();
+        return avg;
+    }
+
+    public Vector2 Cohesion(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+        Vector2 avg = Vector2.zero;
+        foreach (Boid b in insideRadiusBoids)
+            avg += b.currentPosition;
+        avg /= insideRadiusBoids.Count;
+        return (avg - boid.currentPosition).normalized;
+    }
+
+    public Vector2 Separation(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+        Vector2 avg = Vector2.zero;
+        foreach (Boid b in insideRadiusBoids)
+            avg += (b.currentPosition - boid.currentPosition);
+        avg /= insideRadiusBoids.Count;
+        avg *= -1;
+        avg.Normalize();
+        return avg;
+    }
+
+    public Vector2 Direction(Boid boid, GameObject target) {
+      
+        return ((Vector2)target.transform.position - boid.currentPosition).normalized;
+    }
+
+    public List<Boid> GetInsideRadiusBoids(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = new List<Boid>();
+        foreach (Boid b in boids)
+            if (boid.circleCollider2D.OverlapPoint(b.currentPosition))
+                insideRadiusBoids.Add(b);
+        return insideRadiusBoids;
+    }
+}
